87e0f3a754f195013f86255712a70d7f8bd5ecbb
Parth <parth.nadiyapara125626@marwadiuniversity.ac.in>
2026-06-02 09:06:10 +0530

    Add files via upload

---
+++ 2nd.php
@@ -0,0 +1,33 @@
+<?php
+
+
+echo "<center>";
+
+
+echo "==================================================<br>";
+echo "MARWADI UNIVERSITY<br>";
+echo "STUDENT RESULT<br>";
+echo "==================================================<br><br>";
+
+
+echo "Roll No: 7071<br>";
+echo "Name: Parth Nadiyapara<br>";
+echo "Semester: 2nd Semester<br>";
+echo "--------------------------------------------------<br><br>";
+
+
+echo "PHP Programming: 88 / 100<br>";
+echo "Database Systems: 92 / 100<br>";
+echo "Data Structures: 85 / 100<br>";
+echo "--------------------------------------------------<br><br>";
+
+
+echo "Total Marks: 265 / 300<br>";
+echo "Percentage: 88.33%<br>";
+echo "Result Status: PASSED<br>";
+echo "==================================================<br>";
+
+
+echo "</center>";
+
+?>
